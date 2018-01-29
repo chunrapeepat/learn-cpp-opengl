46cafebcf3d59b9fca1c5f5e316cd654ecc85eba
chunza2542 <chunza2542@hotmail.com>
2018-01-29 08:59:41 +0700

    Initial Commit

---
+++ OpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL
+//
+//  Created by Chun Rapeepat on 29/1/2561 BE.
+//  Copyright © 2561 chunza2542. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

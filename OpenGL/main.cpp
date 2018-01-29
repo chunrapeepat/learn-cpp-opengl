91d973e6779d315c60ab4f33e15fe0419b3dc63d
chunza2542 <chunza2542@hotmail.com>
2018-01-29 09:13:53 +0700

    :tada: Initialize project

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -1,11 +1,4 @@
-//
-//  main.cpp
-//  OpenGL
-//
-//  Created by Chun Rapeepat on 29/1/2561 BE.
-//  Copyright © 2561 chunza2542. All rights reserved.
-//
-
+#include <GL/glew.h>
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
